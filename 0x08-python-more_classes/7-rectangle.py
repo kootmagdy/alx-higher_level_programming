2a7010194c9179473bd23bc86f63647b42192849
kootmagdy <kootmagdy5@gmail.com>
2023-09-04 17:40:31 -0700

    b

--- 0x08-python-more_classes/7-rectangle.py
+++ 0x08-python-more_classes/7-rectangle.py
@@ -8,6 +8,7 @@ class Rectangle:
     """Representation of a rectangle"""
 
     number_of_instances = 0
+    print_symbol = "#"
 
     def __init__(self, width=0, height=0):
         """Initializes the rectangle"""
